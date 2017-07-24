2b75ec84987c10e2793266431e7d45f80d583ca0
Maris Mols <maris.mols@redboxdigital.com>
2017-07-24 20:55:33 +0300

    Change module vendor name

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Mmols_CustomerAuthByUsername',
+    'Molsm_CustomerAuthByUsername',
     __DIR__
 );
